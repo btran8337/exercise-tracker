a801b6ff1991a627f4851eedc88109cae0aa71b1
btran8337 <btran8337@yahoo.com>
2019-08-05 20:43:50 -0700

    fixxx

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css"
 
+
 import Navbar from "./components/Navbar";
 import ExercisesList from "./components/ExercisesList";
 import EditExercise from "./components/EditExercise";

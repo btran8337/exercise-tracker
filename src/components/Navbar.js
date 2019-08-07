cd283dc7e648bfd11e1279dc58a2f7fc906fb4d1
btran8337 <btran8337@yahoo.com>
2019-08-06 22:21:44 -0700

    finished components and crud features

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-class Navbar extends Component {
+export default class Navbar extends Component {
+
   render() {
     return (
-      <div>
-        <h3>Navbar component</h3>
-      </div>
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <Link to="/" className="navbar-brand">ExcerTracker</Link>
+        <div className="collpase navbar-collapse">
+        <ul className="navbar-nav mr-auto">
+          <li className="navbar-item">
+          <Link to="/" className="nav-link">Exercises</Link>
+          </li>
+          <li className="navbar-item">
+          <Link to="/create" className="nav-link">Create Exercise Log</Link>
+          </li>
+          <li className="navbar-item">
+          <Link to="/user" className="nav-link">Create User</Link>
+          </li>
+        </ul>
+        </div>
+      </nav>
     );
   }
 }
-
-export default Navbar;
